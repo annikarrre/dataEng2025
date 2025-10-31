98b3d50adac641f4163a210ba81a5201c5b490c8
Agnes <agnes92@ut.ee>
2025-10-31 16:00:15 +0200

    Added dim_date to gold layer

---
+++ dbt/models/gold/dim_date.sql
@@ -0,0 +1,23 @@
+WITH months AS (
+    SELECT
+        toStartOfMonth(addMonths('2021-01-01', number)) AS month_start
+    FROM numbers(60)  -- 60 months = 5 years (2021–2025)
+)
+
+SELECT
+    formatDateTime(month_start, '%Y%m') AS date_id,
+    month_start,
+    toYear(month_start) AS year,
+    toMonth(month_start) AS month,
+    toQuarter(month_start) AS quarter,
+    formatDateTime(month_start, '%M') AS month_name,
+    concat(toString(toYear(month_start)), '-', formatDateTime(month_start, '%m')) AS year_month_label,
+    CASE
+        WHEN toMonth(month_start) IN (12, 1, 2) THEN 'Winter'
+        WHEN toMonth(month_start) IN (3, 4, 5) THEN 'Spring'
+        WHEN toMonth(month_start) IN (6, 7, 8) THEN 'Summer'
+        WHEN toMonth(month_start) IN (9, 10, 11) THEN 'Autumn'
+        ELSE 'Unknown'
+    END AS season
+FROM months
+ORDER BY month_start
