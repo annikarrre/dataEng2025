5351958f5c2420f40ccafdf3dba746d2b0779320
Annika Remmelgas <annikaremmelgas8@gmail.com>
2025-11-02 14:13:30 +0200

    sunshine fixed

--- dbt/models/gold/dim_sunshine.sql
+++ dbt/models/gold/dim_sunshine.sql
@@ -2,22 +2,36 @@
 
 WITH monthly_agg AS (
     SELECT
-        toStartOfMonth(date) AS month_start,
-        round(avg(sunshine_hours), 2) AS avg_sunshine
+        toStartOfMonth(date) AS month_start
     FROM model_silver.sunshine_cleaned
     WHERE toYear(date) >= 2021
     GROUP BY toStartOfMonth(date)
-    HAVING avg_sunshine IS NOT NULL
+),
+avg_station_month AS (
+    SELECT
+        month_start,
+        avg(station_month_total_hours) AS avg_station_month
+    FROM (
+        SELECT
+            toStartOfMonth(date) AS month_start,
+            staid,
+            sum(sunshine_hours)  AS station_month_total_hours
+        FROM model_silver.sunshine_cleaned
+        GROUP BY month_start, staid
+    )
+    GROUP BY month_start
 )
 
 SELECT
     cityHash64(
         concat(
-            toString(month_start),
-            toString(avg_sunshine)
+            toString(m.month_start),
+            toString(round(a.avg_station_month, 2))
         )
     ) AS sunshine_id,
-    formatDateTime(month_start, '%Y%m') AS date_id,
-    avg_sunshine,
-FROM monthly_agg
-ORDER BY month_start
+    formatDateTime(m.month_start, '%Y%m') AS date_id,
+    round(a.avg_station_month, 2) AS avg_station_monthly_hours
+FROM monthly_agg AS m
+LEFT JOIN avg_station_month AS a
+    ON a.month_start = m.month_start
+ORDER BY m.month_start
