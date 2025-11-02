e1a24aa4590094ed8261a8cbdbef956475e71d9a
Annika Remmelgas <annikaremmelgas8@gmail.com>
2025-11-02 13:58:10 +0200

    fixed sunshine gold

--- dbt/models/gold/dim_sunshine.sql
+++ dbt/models/gold/dim_sunshine.sql
@@ -1,7 +1,23 @@
+{{ config(materialized='incremental', database='gold', unique_key='sunshine_id') }}
+
+WITH monthly_agg AS (
+    SELECT
+        toStartOfMonth(date) AS month_start,
+        round(avg(sunshine_hours), 2) AS avg_sunshine
+    FROM model_silver.sunshine_cleaned
+    WHERE toYear(date) >= 2021
+    GROUP BY toStartOfMonth(date)
+    HAVING avg_sunshine IS NOT NULL
+)
+
 SELECT
-  toStartOfMonth(date)                       AS month_start,
-  round(avg(sunshine_hours), 2)              AS avg_daily_sunshine_hours_estonia
-FROM {{ ref('sunshine_cleaned') }}
-WHERE date >= toDate('2021-01-01')
-GROUP BY month_start
+    cityHash64(
+        concat(
+            toString(month_start),
+            toString(avg_sunshine)
+        )
+    ) AS sunshine_id,
+    formatDateTime(month_start, '%Y%m') AS date_id,
+    avg_sunshine,
+FROM monthly_agg
 ORDER BY month_start
